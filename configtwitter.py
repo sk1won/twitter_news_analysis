c1f63a7c3f881c251e1038208cafeab708009616
unknown <sam.sk.won1@gmail.com>
2018-06-30 17:26:50 -0400

    twitter news analysis

---
+++ configtwitter.py
@@ -0,0 +1,5 @@
+# Add your API key
+api_key = "wbREnjYTT3acFDDGvLaD88BTf"
+api_s_key = "uPwNaXthv579aRAu9a1Pn2ycQYOHsA0qhILGa8UUn9j3Q6FSXL"
+access_token = "1009237996531519489-9naVPhEGxrZo0Ee4xa9Ov2gJm62olg"
+access_s_token = "1PXywUIOiep81POaVmm9Zv45gV98Kz0OiaT1c2rv4u8IM"
